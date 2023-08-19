a9989a06a3e0cb2a976c81618a5c5d067e124ac5
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-08-19 07:26:46 +0000

    save

---
+++ 03_bacis/01_function.js
@@ -0,0 +1,114 @@
+//function
+function saymyname(){
+    console.log("hello")
+}
+saymyname();
+saymyname
+
+//function calling
+//reference saymyname
+//calling  saymyname()
+
+// function addtwonumber(number1,number2){
+//    console.log(number1+number2)
+// }
+// const result=addtwonumber(7,null)
+
+
+
+
+
+
+// function addtwonumber(number1,number2){
+//    let result=number1+number2
+//    return result
+// }
+// const result=addtwonumber(7,null)
+// console.log(result)
+
+
+
+
+
+
+// function loginuser(username=){
+//     if(username===undefined){
+//         console.log("please enter a username")
+//         return 
+//     }
+//     return `${username}just log in`
+// }
+
+// console.log(loginuser())
+
+
+
+
+
+
+
+//this is default valu
+// function pk(username="pk")
+
+
+
+
+// //spred operatior take all values enter by user and return the array
+// function calculatecartprice(...num1){
+//     return num1;
+
+// }
+// console.log(calculatecartprice(100,200,300,400))
+
+
+
+
+// const user={
+//     username:"hitesh",
+//     price:400
+// }
+
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+// handleobject(user)
+
+
+
+//obj in function
+// handleobj({
+//     username:"prakhar",
+//     price:399
+// })
+
+
+
+//array in function
+// const mynewarray=[200,400,100,2300]
+
+// function returnsecondvalue(getarray){
+//     return getarray[1]
+// }
+// console.log(returnsecondvalue(mynewarray))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function print(parameter){
+
+//}
+//print(agruments)
+
