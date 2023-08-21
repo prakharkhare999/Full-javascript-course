e37f8197bc1cb6cbbb6b1940fbfd0c7d3a9b1c3a
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-08-21 04:49:13 +0000

    save

--- 03_bacis/03_arrow.js
+++ 03_bacis/03_arrow.js
@@ -1,10 +1,76 @@
-const user={
-    username:"hitesh",
-    price:99,
+// const user={
+//     username:"hitesh",
+//     price:99,
 
 
-    welcomeMassage:function(){
-        console.log(`${this.username},welcome to the website`)
-    }
+//     welcomeMassage:function(){
+//         console.log(`${this.username},welcome to the website`)
+//     }
 
-}
+// }
+// user.welcomeMassage();
+// user.username="prakhar";
+// user.welcomeMassage();
+
+
+// console.log(this);
+
+
+
+// this does not work in the functions
+
+// function chai(){
+//     let username ="hitesh"
+//     console.log(this);
+// }
+
+// chai()
+
+
+
+
+
+// const chai = function(){
+//     let username="hitesh"
+//     console.log(this.username)
+
+// }
+
+
+
+
+
+
+// const chai=()=>{
+//     let username="hitesh"
+//     console.log(this)
+
+// }
+// chai()
+
+
+
+
+//explicit return
+// const addtwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addtwo(3,4))
+
+
+
+//implicit return with return single line
+// const addtwo=(num1,num2)=>num1+num2
+
+
+
+
+//in object
+// const addtwo=(num1,num2)=>({username:"hitesh"})
+
+
+
+
+
+
+
